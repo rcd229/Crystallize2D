64176a5229a34c0f8063c838ca3e95411b19a396
rcd229 <rcd229@cornell.edu>
2015-11-09 14:36:53 -0500

    working on scene changing

---
+++ Assets/Scripts/ModeSwitcher2D.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class ModeSwitcher2D : MonoBehaviour
+{
+    public GameObject[] prefabs;
+    GameObject currentPrefab;
+    int index;
+
+	public ModeSwitcher2D()
+	{
+        index = 0;
+        currentPrefab = Instantiate<GameObject>(prefabs[index]);
+	}
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Destroy(currentPrefab);
+            index++;
+            currentPrefab = Instantiate<GameObject>(prefabs[index]);
+        }
+    }
+}
