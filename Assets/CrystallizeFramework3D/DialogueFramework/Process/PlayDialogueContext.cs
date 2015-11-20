b95d3a1a64386534e834f3915a93632024fe506a
gabeculbertson <gabeculbertson@gmail.com>
2015-11-20 08:27:26 -0500

    play dialouge

---
+++ Assets/CrystallizeFramework3D/DialogueFramework/Process/PlayDialogueContext.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PlayDialogueContext {
+    public DialogueSequence Dialogue { get; set; }
+    public DialogueElement CurrentElement { get; set; }
+    public List<GameObject> Actors { get; set; }
+
+    public GameObject Actor {
+        get {
+            return Actors.FirstOrDefault();
+        }
+    }
+
+    public PlayDialogueContext(DialogueSequence dialogue, params GameObject[] actors) {
+        Dialogue = dialogue;
+        Actors = actors.ToList();
+    }
+}
--- Assets/CrystallizeFramework3D/DialogueFramework/Process/PlayDialogueProcess.cs
+++ Assets/CrystallizeFramework3D/DialogueFramework/Process/PlayDialogueProcess.cs
@@ -4,7 +4,21 @@ using System;
 using System.Collections.Generic;
 
 public class PlayDialogueProcess : EnumeratorProcess<DialogueSequence, object> {
+    static readonly ProcessFactoryRef<DialogueElement, DialogueElement> LineProcessFactory
+        = new ProcessFactoryRef<DialogueElement, DialogueElement>();
+    static readonly ProcessFactoryRef<IEnumerable<DialogueElement>, DialogueElement> PromptProcessFactory
+        = new ProcessFactoryRef<IEnumerable<DialogueElement>, DialogueElement>();
+
+
+    public static void Initialize<T1, T2>()
+        where T1 : IProcess<DialogueElement, DialogueElement>, new()
+        where T2 : IProcess<IEnumerable<DialogueElement>, DialogueElement>, new() {
+        LineProcessFactory.Set<T1>();
+        PromptProcessFactory.Set<T2>();
+    }
+
     public override IEnumerator<SubProcess> Run(DialogueSequence args) {
-        throw new NotImplementedException();
+        var next = args.Elements.Get(0);
+        yield return Get(LineProcessFactory, args.Elements.Get(0));
     }
 }
